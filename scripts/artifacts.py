eb2b09cc34f8f3890e7e4f41f7f47bf3923c5451
Bob Ippolito <bob@redivi.com>
2016-02-28 11:01:39 -0800

    Script to download artifacts from GitHub

---
+++ scripts/artifacts.py
@@ -0,0 +1,48 @@
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib import urlopen
+
+import io
+import json
+import subprocess
+
+
+def get_json(url):
+    return json.loads(urlopen(url).read().decode('utf-8'))
+
+
+def download_file(src_url, dest_path):
+    print(dest_path)
+    subprocess.call(
+        ['curl', '-L', '-#', '-o', dest_path, src_url])
+
+
+def download_appveyor_artifacts():
+    api_url = 'https://ci.appveyor.com/api'
+    builds = get_json(
+        '{}/projects/etrepum/xattr'.format(api_url))
+
+    for job in builds['build']['jobs']:
+        url = '{api_url}/buildjobs/{jobId}/artifacts'.format(
+            api_url=api_url, **job)
+        for artifact in get_json(url):
+            download_file(
+                '{url}/{fileName}'.format(url=url, **artifact),
+                artifact['fileName'])
+
+
+def download_github_artifacts():
+    release = get_json(
+        'https://api.github.com/repos/xattr/xattr/releases/latest')
+    for asset in release['assets']:
+        download_file(asset['browser_download_url'], 'dist/{name}'.format(**asset))
+
+
+def main():
+    #download_appveyor_artifacts()
+    download_github_artifacts()
+
+
+if __name__ == '__main__':
+    main()
