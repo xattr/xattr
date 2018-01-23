aaa4d8d7c7611104e6de4328b50563e4c29598ff
Bob Ippolito <bob@redivi.com>
2018-01-23 07:57:15 -0800

    more pypy build issue stuff

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,6 +6,8 @@ set -x
 if [[ -n "$PYENV_VERSION" ]]; then
     if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
         brew update && brew upgrade pyenv
+        brew install readline xz
+        export CFLAGS="-I$(xcrun --show-sdk-path)/usr/include"
     fi
     eval "$(pyenv init -)"
     pyenv install --list
