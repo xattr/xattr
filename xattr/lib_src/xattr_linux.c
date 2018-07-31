b2b70902109600693d2d98f3f50c69739c2ed798
chrBrd <chris@binaryspanner.com>
2018-07-31 04:17:21 +0100

    Separate out platform specific source.

---
+++ xattr/lib_src/xattr_linux.c
@@ -0,0 +1,135 @@
+/**
+ * Linux compatibility API.
+ */
+
+#include "Python.h"
+#include <sys/types.h>
+#include <sys/xattr.h>
+
+#define XATTR_XATTR_NOFOLLOW    0x0001
+#define XATTR_XATTR_CREATE      0x0002
+#define XATTR_XATTR_REPLACE     0x0004
+#define XATTR_XATTR_NOSECURITY  0x0008
+
+static ssize_t
+xattr_getxattr(const char *path, const char *name, void *value, ssize_t size,
+               uint32_t position, int options) {
+    if (position != 0 || !(options == 0 || options == XATTR_XATTR_NOFOLLOW)) {
+        return -1;
+    }
+    if (options & XATTR_XATTR_NOFOLLOW) {
+        return lgetxattr(path, name, value, size);
+    } else {
+        return getxattr(path, name, value, size);
+    }
+}
+
+static ssize_t
+xattr_setxattr(const char *path, const char *name, void *value, ssize_t size,
+               uint32_t position, int options) {
+    int nofollow;
+    if (position != 0) {
+        return -1;
+    }
+    nofollow = options & XATTR_XATTR_NOFOLLOW;
+    options &= ~XATTR_XATTR_NOFOLLOW;
+    if (options == XATTR_XATTR_CREATE) {
+        options = XATTR_CREATE;
+    } else if (options == XATTR_XATTR_REPLACE) {
+        options = XATTR_REPLACE;
+    } else if (options != 0) {
+        return -1;
+    }
+    if (nofollow) {
+        return lsetxattr(path, name, value, size, options);
+    } else {
+        return setxattr(path, name, value, size, options);
+    }
+}
+
+static ssize_t
+xattr_removexattr(const char *path, const char *name, int options) {
+    if (!(options == 0 || options == XATTR_XATTR_NOFOLLOW)) {
+        return -1;
+    }
+    if (options & XATTR_XATTR_NOFOLLOW) {
+        return lremovexattr(path, name);
+    } else {
+        return removexattr(path, name);
+    }
+}
+
+static ssize_t
+xattr_listxattr(const char *path, char *namebuf, size_t size, int options) {
+    if (!(options == 0 || options == XATTR_XATTR_NOFOLLOW)) {
+        return -1;
+    }
+    if (options & XATTR_XATTR_NOFOLLOW) {
+        return llistxattr(path, namebuf, size);
+    } else {
+        return listxattr(path, namebuf, size);
+    }
+}
+
+static ssize_t
+xattr_fgetxattr(int fd, const char *name, void *value, ssize_t size,
+                uint32_t position, int options) {
+    if (position != 0 || !(options == 0 || options == XATTR_XATTR_NOFOLLOW)) {
+        return -1;
+    }
+    if (options & XATTR_XATTR_NOFOLLOW) {
+        return -1;
+    } else {
+        return fgetxattr(fd, name, value, size);
+    }
+}
+
+static ssize_t
+xattr_fsetxattr(int fd, const char *name, void *value, ssize_t size,
+                uint32_t position, int options) {
+    int nofollow;
+    if (position != 0) {
+        return -1;
+    }
+    nofollow = options & XATTR_XATTR_NOFOLLOW;
+    options &= ~XATTR_XATTR_NOFOLLOW;
+    if (options == XATTR_XATTR_CREATE) {
+        options = XATTR_CREATE;
+    } else if (options == XATTR_XATTR_REPLACE) {
+        options = XATTR_REPLACE;
+    } else if (options != 0) {
+        return -1;
+    }
+    if (nofollow) {
+        return -1;
+    } else {
+        return fsetxattr(fd, name, value, size, options);
+    }
+}
+
+static ssize_t xattr_fremovexattr(int fd, const char *name, int options) {
+    if (!(options == 0 || options == XATTR_XATTR_NOFOLLOW)) {
+        return -1;
+    }
+    if (options & XATTR_XATTR_NOFOLLOW) {
+        return -1;
+    } else {
+        return fremovexattr(fd, name);
+    }
+}
+
+static ssize_t
+xattr_flistxattr(int fd, char *namebuf, size_t size, int options) {
+    if (!(options == 0 || options == XATTR_XATTR_NOFOLLOW)) {
+        return -1;
+    }
+    if (options & XATTR_XATTR_NOFOLLOW) {
+        return -1;
+    } else {
+        return flistxattr(fd, namebuf, size);
+    }
+}
+
+#ifndef XATTR_MAXNAMELEN
+#define XATTR_MAXNAMELEN 127
+#endif
