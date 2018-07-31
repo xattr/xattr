546a5a1ec48283579cbd8bf2d236fe339ee2411b
Bob Ippolito <bob@redivi.com>
2018-07-31 15:04:54 +0000

    v0.9.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 
 from setuptools import setup
 
-VERSION = '0.9.4'
+VERSION = '0.9.5'
 DESCRIPTION = "Python wrapper for extended filesystem attributes"
 LONG_DESCRIPTION = """
 Extended attributes extend the basic attributes of files and directories
